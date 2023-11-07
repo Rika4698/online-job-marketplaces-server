6e4af8cc769f4136ef31cad9385b92b06f52dc0c
Rika4698 <rikasharmin181@gmail.com>
2023-11-07 13:28:10 +0600

    created job collection database using client side data

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 
-console.log(process.env.DB_PASS);
+// console.log(process.env.DB_PASS);
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.i5g3jew.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -25,13 +25,25 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+    const jobCollection = client.db('JobDB').collection("job");
+
+
+    app.post('/jobs', async (req, res) => {
+        const newJob = req.body;
+        console.log(newJob);
+        const result = await jobCollection.insertOne(newJob);
+        res.send(result);
+    })
+
+
+
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
@@ -43,5 +55,5 @@ app.get('/',(req,res) => {
 })
 
 app.listen(port,() => {
-    console.log(`online job marketplaces is running on port ${port}`)
+    console.log(`online job marketplaces is running on port ${port}`);
 })
