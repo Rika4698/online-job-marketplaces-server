1aadd176a2b03752871005da3ab6d6d78d24b02f
Rika4698 <rikasharmin181@gmail.com>
2023-11-11 01:54:37 +0600

    create app get for bid collection sorting in ascending order

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 //middleware
 app.use(cors());
 app.use(express.json());
-
+// {origin:'https://online-job-marketplaces-client.web.app'}
 
 // console.log(process.env.DB_PASS);
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.i5g3jew.mongodb.net/?retryWrites=true&w=majority`;
@@ -28,7 +28,11 @@ async function run() {
     // await client.connect();
     const jobCollection = client.db('JobDB').collection("job");
     const bidCollection = client.db('MyBidDB').collection("bids");
+    //auth api
 
+
+
+     //service api
     app.get('/jobs', async (req, res) => {
         const cursor = jobCollection.find();
         const result = await cursor.toArray();
@@ -44,6 +48,13 @@ async function run() {
     })
     app.get('/bids', async (req, res) => {
       const cursor = bidCollection.find();
+      // const sortedBids = await bidCollection.find().sort({ status: 1 }).toArray();
+      const result = await cursor.toArray();
+      res.send(result);
+  })
+    app.get('/my-bids', async (req, res) => {
+      const cursor = bidCollection.find().sort({ status: 1 });
+      // const sortedBids = await bidCollection.find().sort({ status: 1 }).toArray();
       const result = await cursor.toArray();
       res.send(result);
   })
